8b2165b2e8f780df19a42eed48e7baee3a880549
Gautam Dey <gautam.dey77@gmail.com>
2019-10-26 22:39:04 -0700

    [speech] Made speech class that cache audio files
    Made a Speech class that will cache the audio files, and let the
    program continue processing while playing the audio. When a new
    file is requested to be played the old one is stopped and a the
    file is played.
    
    NOTE: I have not tested this for compile or runtime errors

--- speech.py
+++ speech.py
@@ -2,6 +2,45 @@ import simpleaudio as sa
 import os
 
 
+class Speech:
+    def __init__(self, filePath, *audioFiles):
+        # we are going to load all the files into player objects
+        self.filePath = filePath
+        self._players = {}
+        for file in list(audioFiles):
+            self._players[file] = sa.WaveObject.from_wave_file(so.path.join(filePath,filename)
+            self._last_audio = self._players[file]
+
+    def load(filename):
+            self._players[filename] = sa.WaveObject.from_wave_file(so.path.join(filePath,filename)
+            return self._players[filename]
+
+    def play(filename):
+
+        self._last_audio.stop()
+        if filename in self._players.keys():
+            ply = self._players[filename]
+        else:
+            ply = self.load(filename)
+        ply.play()
+        self._last_audio = ply
+
+    def wait_done(filename):
+        if filename in self._players.keys():
+            self._players[filename].wait_done()
+        else:
+            load(filename).wait_done()
+
+    def play_to_end(filename):
+        if filename in self._players.keys():
+            ply = self._players[filename]
+        else:
+            ply = self.load(filename)
+        ply.play()
+        self._last_audio = ply
+        ply.wait_done()
+
+
 def play_audiofile(filename):
     filePath = os.path.join(os.getcwd(), 'bin', 'audio', filename)
     wave_obj = sa.WaveObject.from_wave_file(filePath)
